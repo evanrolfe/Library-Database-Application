709398a674bca90647e3486c2d5ea4b2a265c1f4
Patrick Rose <pjr0911025@googlemail.com>
2012-12-11 14:31:40 +0000

    Fixed a couple of for loops to be short and sweet

--- Borrower.java
+++ Borrower.java
@@ -63,15 +63,12 @@ public class Borrower
 	{
 		//Initialise array list of Copy, to be returned which will contain the copies on loan.
 		ArrayList<Copy> copies_on_loan = new ArrayList<Copy>();
-		Loan loan;
 		
 		//This iterates through each of the loans associated to the borrower using getLoans method.
-		for(int i=0; i<this.getLoans().size(); i++)
+		for(Loan loan: getLoans())
 		{
-			loan = this.getLoans().get(i);
-			
 			//If the loan associated to the borrower is not overdue then it is added to the array list.
-			if(loan.overDue()==false)
+			if(!loan.overDue())
 			{
 				copies_on_loan.add(loan.copy);				
 			}
@@ -90,18 +87,12 @@ public class Borrower
      */
 	public ArrayList<Copy> getCopiesOverDue() throws Exception
 	{
-		//Initialises a borrower from the database and the overdue copies array list which will be returned.
-		Borrower b = Database.find_borrower(id);
 		ArrayList<Copy> copies_overdue = new ArrayList<Copy>();
-
-		Loan loan;
 		//This iterates through each of the loans associated to the borrower using getLoans method.
-		for(int i=0; i<this.getLoans().size(); i++)
+		for(Loan loan:getLoans())
 		{
-			loan = this.getLoans().get(i);
-
 			//If the loan associated to the borrower is overdue then it is added to the array list.
-			if(loan.overDue()==true)
+			if(loan.overDue())
 			{
 				copies_overdue.add(loan.copy);
 			}
@@ -140,8 +131,6 @@ public class Borrower
 		return overDueLoans.size() > 0;
 	}
 
-	//Calculates any fines the borrower is required to pay.
-
     /**
      * Returns the how much of a fine the borrower owes
      * @return The amount owed by the borrower
@@ -168,48 +157,18 @@ public class Borrower
 		ArrayList<Loan> overDueLoans = new ArrayList<Loan>();
 
 		//Iterate thorough the array list of loans the borrower holds.
-		for(int i=0; i<loans.size(); i++)
-		{
-			//If any loan is overdue it is added to the overDueLoans array list
-			if(loans.get(i).overDue() == true)
-				overDueLoans.add(loans.get(i));
-		}
+        for (Loan loan : loans)
+        {
+            //If any loan is overdue it is added to the overDueLoans array list
+            if (loan.overDue())
+            {
+                overDueLoans.add(loan);
+            }
+        }
 
 		//The array list of overdue loans is returned.
 		return overDueLoans;
 	}
 
-	//This uses a given Key parameter to establish what field is being used in other search queries.
-
-    /**
-     * TODO: Find out who wrote this.
-     * @param key
-     * @return
-     */
-    public Object getValue(String key)
-	{
-		//Initialise the object that will be returned corresponding to a particular search key.
-		Object out = new Object();
-
-		//Establish which filed the given key is in regards to the fields a borrower can be searched on.
-		if(key.equals(this.id))
-		{
-			out = this.id;
-		}
-		else if(key.equals(this.forename))
-		{
-			out = this.forename;
-		}
-		else if(key.equals(this.surname))
-		{
-			out = this.surname;
-		}
-		else if(key.equals(this.email))
-		{
-			out = this.email;
-		}
-		//Returns the object key which is the field that a search is being carried out for on the borrower.
-		return out;
-	}
 }
 
